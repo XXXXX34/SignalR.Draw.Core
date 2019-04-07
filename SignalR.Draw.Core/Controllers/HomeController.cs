9306d2741f0d8e4a2ce772008faa29d5f63ed3d1
XXXXX34 <503025565@qq.com>
2019-04-07 12:17:25 +0800

    hg

--- SignalR.Draw.Core/Controllers/HomeController.cs
+++ SignalR.Draw.Core/Controllers/HomeController.cs
@@ -23,14 +23,6 @@ namespace SignalR.Draw.Core.Controllers
         {
             return View();
         }
-        public IActionResult Index1()
-        {
-            return View();
-        }
-        public IActionResult Index2()
-        {
-            return View();
-        }
 
         public IActionResult Privacy()
         {
