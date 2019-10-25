7a02a646643872e3b7d71a40e38b1b7c534c915b
XXXXX34 <503025565@qq.com>
2019-10-25 08:55:31 +0800

    Revert "33"
    This reverts commit c20b5f76cca5415fff7210f01a3612eeed9730c8.

--- SignalR.Draw.Core/Controllers/HomeController.cs
+++ SignalR.Draw.Core/Controllers/HomeController.cs
@@ -10,8 +10,8 @@ using SignalR.Draw.Core.SignalR;
 
 namespace SignalR.Draw.Core.Controllers
 {
-    //3
-    //3
+    //2
+    //2
     public class HomeController : Controller
     {
         private readonly IHubContext<ChatHub> _chatHub;
